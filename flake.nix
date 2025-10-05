e7da773119873f0f6fa84dca96168aef2ee327c4
Krecikowa <krecikowa01@gmail.com>
2025-10-05 17:28:44 +0200

    change flake
    repo

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
-  description = "Moja konfiguracja NixOS";
+  description = "NixOs config - Krecikowa";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   };
 
   outputs = { self, nixpkgs }: {
