49a29237e83059b724c3702ac230c5ea6373731c
Krecikowa <krecikowa01@gmail.com>
2025-10-05 18:46:13 +0200

    comment

--- home.nix
+++ home.nix
@@ -6,19 +6,19 @@
   home.stateVersion = "25.05";
 
   # Podstawowa konfiguracja git
-  programs.git = {
-    enable = true;
-    userName = "Krecikowa";
-    userEmail = "krecikowa01@gmail.com";  
-  };
+  #  programs.git = {
+  # enable = true;
+  # userName = "Krecikowa";
+  # userEmail = "krecikowa01@gmail.com";  
+  #};
 
   # Bash aliases
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      ll = "ls -la";
-      rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config";
-      update = "cd ~/nixos-config && nix flake update && rebuild";
-    };
-  };
+#programs.bash = {
+#   enable = true;
+#   shellAliases = {
+#     ll = "ls -la";
+#     rebuild = "sudo nixos-rebuild switch --flake ~/nixos-config";
+  #     update = "cd ~/nixos-config && nix flake update && rebuild";
+  # };
+  #};
 }
