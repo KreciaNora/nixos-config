ef0b2e0d9eda0de0c34f53fe4f1ef8833d02d71c
Krecikowa <krecikowa01@gmail.com>
2025-10-05 18:30:12 +0200

    Add one line in home.nix

--- home.nix
+++ home.nix
@@ -4,6 +4,7 @@
   home.username = "krecikowa";
   home.homeDirectory = "/home/krecikowa";
   home.stateVersion = "25.05";
+  home-manager.backupFileExtension = "backup";
 
   # Podstawowa konfiguracja git
   programs.git = {
