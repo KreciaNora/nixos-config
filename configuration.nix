2da9350454f3496fb2979239e3f8fc28b10e2aa1
Krecikowa <krecikowa01@gmail.com>
2025-10-09 14:10:23 +0200

    Update

--- configuration.nix
+++ configuration.nix
@@ -1,20 +1,20 @@
-{ config, pkgs, ... }:
 
-{
+_: {
   imports = [
     ./hardware-configuration.nix
-    ./modules/nvidia.nix
-    ./modules/hyprland.nix
-    ./modules/gaming.nix
-    ./modules/bootloader.nix
-    ./modules/network.nix
-    ./modules/locale.nix
-    ./modules/desktop.nix
-    ./modules/audio.nix
-    ./modules/user.nix
-    ./modules/packages.nix
-    ./modules/hardwere.nix
-    ./modules/nix.nix
+    #./modules/nvidia.nix
+    #./modules/hyprland.nix
+    #./modules/gaming.nix
+    #./modules/bootloader.nix
+    #./modules/network.nix
+    #./modules/locale.nix
+    #./modules/desktop.nix
+    #./modules/audio.nix
+    #./modules/user.nix
+    #./modules/packages.nix
+    #./modules/hardwere.nix
+    # ./modules/nix.nix
+    ./modules
 
   ];
 
---
+++ modules/default.nix
@@ -0,0 +1,16 @@
+_: {
+  imports = [ 
+    ./nix.nix
+    ./audio.nix
+    ./user.nix
+    ./gaming.nix
+    ./locale.nix
+    ./nvidia.nix
+    ./desktop.nix
+    ./network.nix
+    ./hardwere.nix
+    ./hyprland.nix
+    ./packages.nix
+    ./bootloader.nix
+  ];
+}
