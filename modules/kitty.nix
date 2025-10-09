9db02dfd5e8bc7d0ff7675d1fb3e9d3cbf482c8f
Krecikowa <krecikowa01@gmail.com>
2025-10-09 04:35:30 +0200

    kitty.nix change

--- modules/kitty.nix
+++ modules/kitty.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
-
 let
-   environment.etc."xdg/config/kitty/kitty.conf".text = ''
+  kittyConfig = ''
     font_family FiraCode Nerd Font
     font_size 29.0
     background #1e1e2e
@@ -9,15 +8,15 @@ let
     cursor #f5e0dc
     scrollback_lines 10000
     enable_audio_bell no
-
     map ctrl+shift+t new_tab
     map ctrl+shift+w close_tab
   '';
 in {
+  # environment.systemPackages = [ pkgs.kitty ];
+  
   system.activationScripts.kittyConfig.text = ''
     mkdir -p /home/krecikowa/.config/kitty
     echo "${kittyConfig}" > /home/krecikowa/.config/kitty/kitty.conf
     chown -R krecikowa:krecikowa /home/krecikowa/.config/kitty
   '';
 }
-
