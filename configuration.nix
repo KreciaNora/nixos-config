686c7d31e0c07d7cb86413023544db35c45c3898
Krecikowa <krecikowa01@gmail.com>
2025-10-11 19:09:48 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,21 +1,7 @@
 { config, lib, pkgs, ... }:
 {
   imports = [
-    ./hardware-configuration.nix
-    #./modules/nvidia.nix
-    #./modules/hyprland.nix
-    #./modules/gaming.nix
-    #./modules/bootloader.nix
-    #./modules/network.nix
-    #./modules/locale.nix
-    #./modules/desktop.nix
-    #./modules/audio.nix
-    #./modules/user.nix
-    #./modules/packages.nix
-    #./modules/hardwere.nix
-    # ./modules/nix.nix
     ./modules
-
   ];
 
    fonts.packages = with pkgs; [
